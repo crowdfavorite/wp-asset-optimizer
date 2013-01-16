0580d5610e6011b146030da05d0ccc1ed48bf643
Tom Rodenberg <tom@crowdfavorite.com>
2013-01-16 14:19:42 -0700

    Update general and advanced settings templates.

---
+++ views/advanced-settings.php
@@ -0,0 +1,105 @@
+<?php
+/*
+Template: Advanced Settings
+Author: Crowd Favorite
+Author URI: http://crowdfavorite.com
+*/
+?>
+<div class="advanced" style="display:none;">
+	<label for="chk_cfao_using_cache">
+		<input type="checkbox" name="cfao_using_cache" id="chk_cfao_using_cache" value="true" <?php checked($cfao_using_cache, true); ?> />
+		<?php echo esc_html(__('This site is using a site-caching solution.')); ?>
+	</label>
+	<span class="cachehelp-hover">
+		<i class="icon"> O</i>
+		<p class="cachehelp">
+			<?php echo esc_html(__('Select this option if your site is using a plugin like WP Super Cache, or another static caching solution, to ensure that the concatenated files are generated and served before the cache occurs.')); ?>
+		</p>
+	</span>
+	<section id="ao-js" class="section">
+		<div class="main">
+			<div class="section-info">
+				<h3 class="section-header"><?php echo esc_html(__('Javascript')); ?></h3>
+				<span class="last-updated">
+					<?php echo esc_html(__('Latest version:')); ?> <time>12/12/32 at 12:41:11pm</time>
+				</span>
+			</div>
+			<table id="js-table" class="files"  cellspacing="0">
+				<thead>
+					<tr>
+						<th><a href="#"><?php echo esc_html(__('Order')); ?><i class="icon">V</i></a></th>
+						<th class="filename"><a href="#"><?php echo esc_html(__('Name')); ?><i class="icon">V</i></a></th>
+						<th class="center"><?php echo esc_html(__('Compile')); ?>
+							<br />
+							<input type="checkbox" id="js-compile-all" />
+						</th>
+						<th class="center"><?php echo esc_html(__('Minify')); ?>
+							<br />
+							<input type="checkbox" id="js-min-all" >
+						</th>
+					</tr>
+					<tr>
+					</tr>
+				</thead>
+				<?php
+					$index = 1;
+					$tab_type = 'scripts';
+					foreach ($scripts as $handle => $details) {
+						include('script-file.php');
+						$index++;
+					}
+				?>
+			</table>
+			<div>
+				<ul class="js-minset">
+					<li>
+						<input name="js-minify" id="minset-whitespace" type="radio" value="whitespace"<?php checked($minify_js_level, 'whitespace'); ?> />
+						<label for="minset-whitespace">Whitespace only (recommended)</label>
+					</li>
+					<li>
+						<input name="js-minify" id="minset-simple" type="radio" value="simple"<?php checked($minify_js_level, 'simple'); ?> />
+						<label for="minset-simple">Simple (usually works)</label>
+					</li>
+					<li>
+						<input name="js-minify" id="minset-advanced" type="radio" value="advanced"<?php checked($minify_js_level, 'advanced'); ?> />
+						<label for="minset-advanced">Advanced (best performance, but requires strict code use)</label>
+					</li>
+				</ul>
+			</div>
+			<button id="js-obliterate" class="button" name="cfao_save_settings" value="clear_scripts_cache"><?php echo esc_html(__('Obliterate JS Files')); ?></button>
+		</div>
+	</section>
+	<section id="ao-css" class="section">
+		<div class="main">
+			<div class="section-info">
+				<h3 class="section-header">CSS</h3>
+				<span class="last-updated">
+					Latest version: <time>12/12/32 at 12:41:11pm</time>
+				</span>
+			</div>
+			<table id="css-table" class="files"  cellspacing="0">
+				<thead>
+					<tr>
+						<th><a href="#">Order<i class="icon">V</i></a></th>
+						<th class="filename"><a href="#">Name<i class="icon">V</i></a></th>
+						<th class="center">Compile
+							<br />
+							<input type="checkbox" id="css-compile-all" />
+						</th>
+					</tr>
+					<tr>
+					</tr>
+				</thead>
+				<?php
+					$index = 1;
+					$tab_type = 'styles';
+					foreach ($styles as $handle => $details) {
+						include ('script-file.php');
+						$index++;
+					}
+				?>
+			</table>
+			<button id="css-obliterate" class="button" name="cfao_save_settings" value="clear_styles_cache"><?php echo esc_html(__('Obliterate CSS Files')); ?></button>
+		</div>
+	</section>
+</div>
--- views/general-settings.php
+++ views/general-settings.php
@@ -5,42 +5,20 @@ Author: Crowd Favorite
 Author URI: http://crowdfavorite.com
 */
 ?>
-<div class="tab" id="cf-asset-optimizer-settings-general">
-	<h2 class="tab-title"><?php echo esc_html(__('General Settings')); ?></h2>
-	<fieldset id="cf-asset-optimizer-general-settings">
-		<legend><h3><?php echo esc_html(__('General Settings')); ?></h3></legend>
-		<ul>
-			<li>
-				<label for="chk_cfao_using_cache">
-					<input type="checkbox" name="cfao_using_cache" id="chk_cfao_using_cache" value="true"<?php checked($cfao_using_cache, true); ?> />
-					<?php echo esc_html(__('This site is using a site-caching solution.')); ?>
-				</label>
-				<p><?php echo esc_html(__('Select this option if your site is using a plugin like WP Super Cache, or another static caching solution, to ensure that the concatenated files are generated and served before the cache occurs.')); ?></p>
-			</li>
-			<?php do_action('cfao_general_settings_tab_list'); ?>
-		</ul>
-	</fieldset>
-	<fieldset id="cf-asset-optimizer-settings-minify-settings">
-		<legend><h3><?php echo esc_html(__('JavaScript Minification Settings')); ?></h3></legend>
-		<p><?php echo esc_html(__('Minification of JavaScript is done through Google\'s Closure Compiler. Levels of minification available are listed below.')); ?></p>
-		<ul>
-			<li>
-				<input type="radio" name="js-minify" id="cfao-js-minify-none" value=""<?php checked (empty($minify_js_level)); ?> />
-				<label for="cfao-js-minify-none"><?php echo esc_html(__('None')); ?></label>
-			</li>
-			<li>
-				<input type="radio" name="js-minify" id="cfao-js-minify-wsonly" value="whitespace"<?php checked($minify_js_level, 'whitespace'); ?> />
-				<label for="cfao-js-minify-wsonly"><?php echo esc_html(__('Whitespace only (recommended)')); ?></label>
-			</li>
-			<li>
-				<input type="radio" name="js-minify" id="cfao-js-minify-simple" value="simple"<?php checked($minify_js_level, 'simple'); ?> />
-				<label for="cfao-js-minify-simple"><?php echo esc_html(__('Simple (usually works)')); ?></label>
-			</li>
-			<li>
-				<input type="radio" name="js-minify" id="cfao-js-minify-advanced" value="advanced"<?php checked($minify_js_level, 'advanced'); ?> />
-				<label for="cfao-js-minify-advanced"><?php echo esc_html(__('Advanced (best performance, requires strict code use)')); ?></label>
-			</li>
-		</ul>
-	</fieldset>
-	<button class="button-primary" name="cfao_save_settings" value="save_general_settings"><?php echo esc_html('Save General Settings', true); ?></button>
+<div class="settings">
+	<div>
+		<input type="radio" name="setting" id="on" value="on"<?php checked($compile_setting, 'on'); ?> />
+		<label class="option" for="on">ON</label>
+		<span>| <?php echo esc_html(__('This will automatically recompile all CSS and JS files whenever they change.')); ?></span>
+	</div>
+	<div>
+		<input type="radio" name="setting" id="off" value="off"<?php checked($compile_setting, 'off'); ?> />
+		<label class="option" for="off">OFF</label>
+		<span>| <?php echo esc_html(__('This will stop any files from being compiled.')); ?></span>
+	</div>
+	<div>
+		<input type="radio" name="setting" id="custom" value="custom"<?php checked($compile_setting, 'custom'); ?> />
+		<label class="option" for="custom">CUSTOM</label>
+		<span>| <?php echo esc_html(__('This lets you choose what you want compiled and what you want to exclude.')); ?></span>
+	</div>
 </div>
