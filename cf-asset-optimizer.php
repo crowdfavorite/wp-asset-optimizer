66819425a7d1b4bee5f5556a8aa7d0120341acf1
Steven Mathias <steve@crowdfavorite.com>
2013-03-05 10:43:05 -0700

    Cleaned up regex structure for future analysis / improvement.

--- cf-asset-optimizer.php
+++ cf-asset-optimizer.php
@@ -674,9 +674,24 @@ class CFAssetOptimizerStyles {
 							
 								// Update paths that are based on web root.
 							if (count($parts) > 1) {
-								$src = preg_replace('#url\s*\(\s*(["\']?)\s*(/[^[:space:]|data:].+?)\s*\1\s*\)#x',
-									'url('.$parts[1].'$2)', $src
-								);
+								$regex = '~
+										url\s*\(             # url( with optional internal whitespace
+										\s*                  # optional whitespace
+										(                    # begin group 1
+										  ["\']?             #   an optional single or double quote
+										)                    # end option group 1
+										\s*                  # optional whitespace
+										(                    # begin option group 2
+										  /                  #     url starts with / for web root url
+										  [^[:space:]]       #     one single non-space character
+										  .+?                #     one or more (non-greedy) any character
+										)                    # end option group 2
+										\s*                  # optional whitespace
+										\1                   # match opening delimiter
+										\s*                  # optional whitespace
+										\)                   # closing )
+									~x';
+								$src = preg_replace($regex,'url('.$parts[1].'$2)', $src);
 							}
 								// Update paths based on script location
 							if (count($parts) > 2) {
@@ -708,9 +723,7 @@ class CFAssetOptimizerStyles {
 									  \s*                  # optional whitespace
 									  \)                   # closing )
 									  ~x';
-								$src = preg_replace($regex,
-									'url('.$parts[1].$parts[2].'$2)', $src
-								);
+								$src = preg_replace($regex, 'url('.$parts[1].$parts[2].'$2)', $src);
 							}
 							
 							$style_file_src .= $src . "\n";
