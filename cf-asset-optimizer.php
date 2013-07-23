9a13d793747f520268792dac19d01fb847ea6536
Steven Mathias <smathias@crowdfavorite.com>
2013-07-23 10:04:10 -0600

    Updated version and changelog.

--- cf-asset-optimizer.php
+++ cf-asset-optimizer.php
@@ -4,7 +4,7 @@ Plugin Name: CF Asset Optimizer
 Plugin URI: http://crowdfavorite.com
 Description: Used to serve optimized and concatenated JS and CSS files enqueued on a page.
 Author: Crowd Favorite
-Version: 1.2
+Version: 1.2-dev
 Author URI: http://crowdfavorite.com
 */
 
